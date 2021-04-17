cf8ede9b033d4e77802b6e606e083abc153a77a9
vandicleydson@gmail.com <vandicleydson@gmail.com>
2021-04-17 19:55:44 -0300

    Primeiras Variaveis

---
+++ src/components/navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Navigation (){
+    return (<nav className="site-navigation">
+        <span>Multiverso Dev</span>
+        <ul>
+            <li>
+                
+            </li>
+        </ul>
+        </nav>)
+}
